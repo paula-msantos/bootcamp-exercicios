a57827dc77cf45eb30532c8d48e78bd5649b96a0
Paula Líbia Santos <paula.msantos@mercadolivre.com>
2022-10-04 17:53:54 -0300

    incrementando o codigo com PathVariable

--- Spring01/src/main/java/br/com/dh/spring01/controller/HelloWorldController.java
+++ Spring01/src/main/java/br/com/dh/spring01/controller/HelloWorldController.java
@@ -1,16 +1,18 @@
 package br.com.dh.spring01.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
 
     @GetMapping
-    public String getHello (){
+    public String getHello(){
         return "Olá Mundo!";
+    }
+
+    @GetMapping("/nome/{nome}")
+    public String getHello(@PathVariable String nome){
+        return "Olá " + nome + "!";
     }
 }
