ba2f2d2817b4d235228f9e446d82ee87b2cfe685
Paula Líbia Santos <paula.msantos@mercadolivre.com>
2022-10-04 17:44:34 -0300

    atualizando arquivos do bootcamp-exercicios

---
+++ Spring01/src/main/java/br/com/dh/spring01/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package br.com.dh.spring01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @GetMapping
+    public String getHello (){
+        return "Olá Mundo!";
+    }
+}
