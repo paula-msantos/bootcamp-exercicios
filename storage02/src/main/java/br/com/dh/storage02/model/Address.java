621363c39997a1fbe59fa6f07b02edd9f10918ff
Paula Líbia Santos <paula.msantos@mercadolivre.com>
2022-10-26 11:34:45 -0300

    iniciando o storage02

---
+++ storage02/src/main/java/br/com/dh/storage02/model/Address.java
@@ -0,0 +1,21 @@
+package br.com.dh.storage02.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String location;
+
+}
